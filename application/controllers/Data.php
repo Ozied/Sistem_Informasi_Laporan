51860a5d5f03964c7be5a29b2d5556731cf49c80
Ozied <fauzifayyad@gmail.com>
2025-08-11 12:02:03 +0700

    Listdokumentpelatihan function

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -1164,7 +1164,7 @@ public function detailpelatihan()
         $this->load->view('footer_view',$this->data);
 	}
 
-	public function generateLaporan($id_pelatihan)
+	public function listdokumenpelatihan($id_pelatihan)
 	{
 		$this->data['idbo'] = $this->session->userdata('ses_id');
 
